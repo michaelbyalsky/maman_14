ffb19a9dc09974e2d242db7a7d719f783300cc3e
michaelbyalsky <michael.byalsky@gmail.com>
2023-07-15 10:50:15 +0300

    try

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int main(int argc, char* argv[]) {
     const char* outputFilename = "test_files/after_pre.as";
     pre_assemble(filename, outputFilename);
 
+    const char* filename2 = "test_files/after_pre.as";
+
     return 0;
 }
